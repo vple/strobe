7ef2b2b9f4e5b1d5afa9769907cd386dc4366ece
Vincent P Le <vple@mit.edu>
2013-05-02 11:11:03 -0400

    Added draw function to Line class.

--- line.js
+++ line.js
@@ -1,7 +1,29 @@
-<script>
 (function() {
     var Line = function(prevLine, nextLine, points) {
-    
+        // stores prev and next lines using a doubly linked list structure
+        this.prevLine = prevLine;
+        this.nextLine = nextLine;
+        this.points = points;       // array of points to move to when drawing this line
+                                    // first point should be starting point of line
+        
+        if (prevLine != null) {
+            prevLine.nextLine = this;
+        }
+        if (nextLine != null) {
+            nextLine.prevLine = this;
+        }
     };
+    window.Line = Line;
+    
+    Line.prototype.draw = function(context, red, green, blue) {
+        context.strokeStyle = 'rgb(' + red + ',' + green + ',' + blue + ')';
+        context.beginPath();
+        context.moveTo(this.points[0][0], this.points[0][1]);
+        
+        for (i = 0; i < this.points.length; i++) {
+            context.lineTo(this.points[i][0], this.points[i][1]);
+        }
+        context.stroke();
+    }
+    
 })();
-</script>
