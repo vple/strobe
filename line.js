46edbd31432824eb55a6d9b6fc4f2e266f8a0878
Vincent P Le <vple@mit.edu>
2013-05-02 11:44:05 -0400

    Stores lines drawn.

--- line.js
+++ line.js
@@ -1,10 +1,11 @@
 (function() {
-    var Line = function(prevLine, nextLine, points) {
+    var Line = function(prevLine, nextLine, points, color) {
         // stores prev and next lines using a doubly linked list structure
         this.prevLine = prevLine;
         this.nextLine = nextLine;
         this.points = points;       // array of points to move to when drawing this line
                                     // first point should be starting point of line
+        this.color = color;
         
         if (prevLine != null) {
             prevLine.nextLine = this;
@@ -15,8 +16,8 @@
     };
     window.Line = Line;
     
-    Line.prototype.draw = function(context, red, green, blue) {
-        context.strokeStyle = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    Line.prototype.draw = function(context, color) {
+        context.strokeStyle = 'rgb(' + color[0] + ',' + color[1] + ',' + color[2] + ')';
         context.beginPath();
         context.moveTo(this.points[0][0], this.points[0][1]);
         
